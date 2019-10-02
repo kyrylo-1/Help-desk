e3a7239a284702d63cae9ce7abe03bb55e78a021
Kirill Avramenko <mirecsy@gmail.com>
2019-10-01 17:50:43 -0700

    add empty home page

--- help-desk-client/src/App.js
+++ help-desk-client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Register from './components/auth/Register';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="container">
         <Switch>
-          <Route exact path="/" component={Register} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
         </Switch>
       </div>
     </Router>
