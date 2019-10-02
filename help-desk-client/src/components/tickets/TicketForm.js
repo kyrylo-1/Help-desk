0f7cc9513ceaed74c4077e30496bf1364a9d7f8a
Kirill Avramenko <mirecsy@gmail.com>
2019-10-02 12:13:35 -0700

    implemet add ticket without restrictions

--- help-desk-client/src/components/tickets/TicketForm.js
+++ help-desk-client/src/components/tickets/TicketForm.js
@@ -3,26 +3,60 @@ import TicketContext from '../../context/ticket/ticketContext';
 
 const TicketForm = () => {
   const ticketContext = useContext(TicketContext);
-  const { addTicket } = ticketContext;
-  const [description, setDescription] = useState('');
+  const { addTicket, current } = ticketContext;
 
-  const onChange = e => setDescription(e.target.value);
+  useEffect(() => {
+    if (current == null) {
+      // setDescription('');
+    }
+  }, [current]);
+
+  const [ticket, setDescription] = useState({
+    description: ''
+  });
+
+  const { description } = ticket;
+
+  const onChange = e =>
+    setDescription({ ...ticket, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
     console.log('onSubmit');
+    if (current === null) {
+      addTicket(ticket);
+    }
+  };
+
+  const clearAll = () => {
+    console.log('clear all');
   };
 
   return (
     <form onSubmit={onSubmit}>
-      <h2 className="text-primary">{'Add Ticket'}</h2>
+      <h2 className="text-primary">{current ? 'Edit Ticket' : 'Add Ticket'}</h2>
       <input
         type="text"
         placeholder="write description"
         name="description"
         value={description}
         onChange={onChange}
+        required
       />
+      <div>
+        <input
+          type="submit"
+          value={current ? 'Edit' : 'Add'}
+          className="btn btn-primary btn-block"
+        />
+      </div>
+      {current && (
+        <div>
+          <button className="btn btn-light btn-block" onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
     </form>
   );
 };
