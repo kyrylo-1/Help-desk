88ebcdb7dd7a58109fead55a24015973a9ce2151
Kirill Avramenko <mirecsy@gmail.com>
2019-10-07 01:06:32 -0700

    add Login tests

--- Help-desk.API/HelpDesk.API.Tests/AuthTests.cs
+++ Help-desk.API/HelpDesk.API.Tests/AuthTests.cs
@@ -6,10 +6,11 @@ using Xunit;
 using HelpDesk.API.Services;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HelpDesk.API.Tests
 {
-    public class AuthTests: TestsBase
+    public class AuthTests : TestsBase
     {
         private User GetSampleUser
         {
@@ -23,14 +24,65 @@ namespace HelpDesk.API.Tests
 
             }
         }
+        private const string SAMPLE_PASS = "password";
+
         [Fact]
         public async void Register_ShouldRegister()
         {
-            AuthRepository authRepo = new AuthRepository(context);
+            var authRepo = new AuthRepository(context);
+            User userToCreate = GetSampleUser;
+            User createdUser = await authRepo.Register(userToCreate, SAMPLE_PASS);
+
+            CheckUser(userToCreate, createdUser, SAMPLE_PASS);
+        }
+
+        [Fact]
+        public async void Register_NotPass_EmptyPass()
+        {
+            var authRepo = new AuthRepository(context);
             User userToCreate = GetSampleUser;
 
-            string inputedPass = "password";
-            User createdUser = await authRepo.Register(userToCreate, inputedPass);
+            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => authRepo.Register(userToCreate, ""));
+            Assert.Equal("Parameters are invalid", ex.Message);
+        }
+
+        [Fact]
+        public async void Register_NotPass_NullUser()
+        {
+            var authRepo = new AuthRepository(context);
+
+            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => authRepo.Register(null, "pass"));
+            Assert.Equal("Parameters are invalid", ex.Message);
+        }
+
+        [Fact]
+        public async void Login_Pass()
+        {
+            var authRepo = new AuthRepository(context);
+            User userToCreate = GetSampleUser;
+
+            await authRepo.Register(userToCreate, SAMPLE_PASS);
+
+            User userLoggedIn = await authRepo.Login(userToCreate.Username, SAMPLE_PASS);
+            CheckUser(userToCreate, userLoggedIn, SAMPLE_PASS);
+        }
+
+        [Fact]
+        public async void Login_Not_Passed()
+        {
+            var authRepo = new AuthRepository(context);
+            User userToCreate = new User
+            {
+                Username = "user-test2",
+                Type = "TeamMember"
+            }; ;
+
+            User user = await authRepo.Login(userToCreate.Username, SAMPLE_PASS);
+            Assert.Null(user);
+        }
+
+        private void CheckUser(User userToCreate, User createdUser, string providedPass)
+        {
             Assert.NotNull(createdUser);
             Assert.Equal(userToCreate.Username, createdUser.Username);
             Assert.True(createdUser.Id > 0);
@@ -41,26 +93,7 @@ namespace HelpDesk.API.Tests
             Assert.NotNull(createdUser.PasswordSalt);
             Assert.NotEmpty(createdUser.PasswordSalt);
 
-            Assert.True(PasswordUtil.VerifyPasswordHash(inputedPass, createdUser.PasswordHash, createdUser.PasswordSalt));
-        }
-
-        [Fact]
-        public async void Register_NotPass_EmptyPass()
-        {
-            AuthRepository authRepo = new AuthRepository(context);
-            User userToCreate = GetSampleUser;
-
-            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => authRepo.Register(userToCreate, ""));
-            Assert.Equal("Parameters are invalid", ex.Message);
-        }
-
-        [Fact]
-        public async void Register_NotPass_NullUser()
-        {
-            AuthRepository authRepo = new AuthRepository(context);
-
-            Exception ex = await Assert.ThrowsAsync<ArgumentException>(() => authRepo.Register(null, "pass"));
-            Assert.Equal("Parameters are invalid", ex.Message);
+            Assert.True(PasswordUtil.VerifyPasswordHash(providedPass, createdUser.PasswordHash, createdUser.PasswordSalt));
         }
     }
 }
